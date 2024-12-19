f6c57d3f518a0b34c1bbc0fc199922917658b18d
youssef elhoubi <elhoubiyoussef@gmail.com>
2024-12-19 15:49:16 +0100

    added the middlwares to validate the roles and the cokies

--- middleware/isAdmine.php
+++ middleware/isAdmine.php
@@ -2,7 +2,7 @@
 if(empty($_COOKIE["userID"])){
     header("Location: ../pages/signIn.php?message=" . urlencode("you are not even loged in "));
 }
-if ($_COOKIE["userID"]!=1) {
+if ($_COOKIE["userID"]!=2) {
     header("Location: ../pages/signIn.php?message=".urldecode("your account can not get to this page "));
 }
 ?>
